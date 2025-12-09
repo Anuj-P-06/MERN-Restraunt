f2f1bc7092a82e346cb8a66f05762459cff8448f
Anuj-P-06 <anujp.02003@gmail.com>
2025-12-09 22:35:06 +0530

    Add routing and context provider to main application structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,34 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import MenuDetails from "./pages/MenuDetails";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import BookTable from "./pages/BookTable";
+import MyBookings from "./pages/MyBookings";
+import MyOrders from "./pages/MyOrders";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 const App = () => {
-  return 
-  <div>
-    App
-  </div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/menu-details/:id" element={<MenuDetails />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/book-table" element={<BookTable />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
